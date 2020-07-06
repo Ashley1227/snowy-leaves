63d2cf9cb046a8703bf41124172ca2c349b3bc14
Ashley1227 <ashley.rued1227@gmail.com>
2020-07-05 22:08:42 -0400

    1.3.1

---
+++ remappedSrc/io/github/ashley1227/snowyleaves/SnowyLeaves.java
@@ -0,0 +1,13 @@
+package io.github.ashley1227.snowyleaves;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SnowyLeaves implements ModInitializer {
+
+//	public static GameRules.RuleKey<BooleanRule> REQUIRES_SKY_ACCESS = Gamerule.register("snowyLeavesRequireSkyAccess", BooleanRule.create(true));
+
+
+	@Override
+	public void onInitialize() {
+	}
+}
